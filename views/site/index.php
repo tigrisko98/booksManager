5f72d7dc31f4a778b9dba8c2a7e39eb09b6cc656
tigrisko98 <kostia.tigrisko@gmail.com>
2021-04-18 13:40:00 +0300

    renamed labels in deleteModal

--- views/site/index.php
+++ views/site/index.php
@@ -20,27 +20,27 @@
                             <td><?php echo $book['date']; ?></td>
                             <td><a href="/book/update/<?php echo $book['id']; ?>"
                                    class="btn btn-primary">Редактировать</a>
-                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Удалить</button>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">Удалить</button>
                             </td>
                         </tr>
                     <?php endforeach; ?>
                 </table>
             <?php endif; ?>
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="deleteModalLabel">Удаление книги</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
 
-                            <p>Вы уверены, что хотите удалить книгу <?php echo $book['title'] ?></p>
+                            <p>Вы уверены, что хотите удалить книгу «<?php echo $book['title'] ?>»?</p>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
                             <form action="/book/delete/<?php echo $book['id']; ?>" method="post">
-                                <button type="submit" name="submit" class="btn btn-primary">Save changes</button>
+                                <button type="submit" name="submit" class="btn btn-danger">Удалить</button>
                             </form>
                         </div>
                     </div>
